b7115bc445d69946b9ab28f3eda87179a0f389c2
FrankM89 <114839168+FrankM89@users.noreply.github.com>
2022-11-12 20:09:19 -0800

    Add password character variables, PW function, if and character requirements statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,35 @@
 // Assignment code here
 
-
-// Get references to the #generate element
+// Variables for password generator
 var generateBtn = document.querySelector("#generate");
+var passwordBase = ""
+var numbers = "0123456789"
+var specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerCase = upperCase.toLowerCase()
+
+var password = [];
+
+// Funciton for password generator
+function generatePassword() {
+  console.log("Click the button")
+
+// Prompts user with questions about generating password options
+
+var length = prompt("How many characters will your password be? Enter a number between 8 and 128")
+console.log(length)
+
+// If statement for password is at least 8 or up to 128 characters
+if (length < 8 || length > 128) {
+
+
+// Prompts user on password length requirements
+ alert("Your password must be between 8 and 128");
+ return generatePassword()
+}
+
+
+
 
 // Write password to the #password input
 function writePassword() {
